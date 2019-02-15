6787ee6ba5228778e35714feada3e14783e227a9
Mira <miralilleholtvik@gmail.com>
2019-02-15 15:41:05 +0100

    Compute pi

--- mach0/M_q1.cpp
+++ mach0/M_q1.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 
-// Are we supposed to return pi or Sn?
-// What is x?
+// Calculate pi by use of the Machin formula with n elements
 double Machin(int n) {
-    double v [n];
+    double x1 = 1/5;
+    double x2 = 1/239;
+    pi = 4*(arctan(x1) - arctan(x2));
+    return pi
+}
+
+
+
+double arctan(double x, int n) {        
     for( int i = 0; i < n; i ++){
         v[i] = (-1)^i * x^(2*(i+1)-1)/(2*(i+1)-1);
     }
--- mach0/RZ_q1.cpp
+++ mach0/RZ_q1.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 
-// Are we supposed to return pi or Sn?
+// Calculate pi by use of Riemann Zeta's formula
 double RiemannZeta(int n) {
     double v [n];
     for( int i = 0; i < n; i ++){
         v[i] = 1/pow(i+1,2);
     }
-    return sum(v)
+    S_n = sum(v)
+    return pow(S_n*6,0.5)
 }
