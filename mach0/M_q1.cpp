7b62f3df8361655af7da06c4822aafe794d61f22
Mira <miralilleholtvik@gmail.com>
2019-02-19 17:07:39 +0100

    change in json file

--- mach0/M_q1.cpp
+++ mach0/M_q1.cpp
@@ -7,15 +7,16 @@ using namespace std;
 double Machin(int n) {
     double x1 = 1/5;
     double x2 = 1/239;
-    pi = 4*(arctan(x1) - arctan(x2));
-    return pi
+    double pi = 4*(arctan(x1,n) - arctan(x2,n));
+    return pi;
 }
 
 
 
-double arctan(double x, int n) {        
+double arctan(double x, int n) {
+    double sum = 0;     
     for( int i = 0; i < n; i ++){
-        v[i] = (-1)^i * x^(2*(i+1)-1)/(2*(i+1)-1);
+        sum += pow(-1,i) * x^(2*(i+1)-1)/(2*(i+1)-1);
     }
-    return sum(v)
+    return sum;
 }
--- mach0/RZ_q1.cpp
+++ mach0/RZ_q1.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 // Calculate pi by use of Riemann Zeta's formula
 double RiemannZeta(int n) {
-    double v [n];
+    double sum = 0;
     for( int i = 0; i < n; i ++){
-        v[i] = 1/pow(i+1,2);
+        sum += 1/pow(i+1,2);
     }
-    S_n = sum(v)
-    return pow(S_n*6,0.5)
+    return pow(sum*6,0.5);
 }
