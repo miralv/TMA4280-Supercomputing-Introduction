eaf7b24f83709d251de450210443360cc9760811
miralv <miralilleholtvik@gmail.com>
2019-03-03 17:17:24 +0100

    mach0 finished

--- mach0/main.cpp
+++ mach0/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <string>
 
@@ -20,7 +21,6 @@ double sum_v_machin(int n, double x){
     return sum;
 }
 
-
 double find_pi_machin(int n){
     // Compute pi by use of the machin formula
     double x1 = 1.0/5;
@@ -31,8 +31,7 @@ double find_pi_machin(int n){
     return pi;
 }
 
-
-int utest(){
+void utest(){
     std::string test_name = "mach0: utest for n =3\n";
 
     double expected_pi = 3.14162102932503461972;
@@ -44,22 +43,58 @@ int utest(){
     
     }//
 
+void writeVerificationToFile(std::string filename, double* error_vec){
+    std::ofstream myfile;
+    myfile.open(filename);
+    if (myfile.is_open()){
+        myfile << "2^n error\n";
+        for (int k = 1; k<=24; k++){
+            myfile<< k <<"  "<<error_vec[k-1]<<"\n";
+        }
+        myfile.close();
+        std::cout<<"Verification error written to file "<<filename<<std::endl;
+    }
+    else std::cout<<"Unable to open file";
+}
+
+void vtest(){
+    std::cout<<"Running vtest"<<std::endl;
+    int n;
+    double err[24];
+    double pi_true = atan(1)*4;
+    for (int k = 1; k<=24; k++){
+        n = pow(2,k);
+        err[k-1] = fabs(pi_true - find_pi_machin(k)); 
+    }
+    writeVerificationToFile("mach0_vtest.txt", err);
+} 
+
+void printOptions(){
+    std::cout<<"argv[1]=n\nOptional parameters:"<<std::endl;
+    std::cout<<"argv[2]=1: run unit test"<<std::endl;
+    std::cout<<"argv[3]=1: run verification test\n";
+}
+
+
 int main(int argc, char** argv){
-    std::string message = "";
-
-
     if(argc<2) {
         std::cout<<"Requires arguemnt: n"<<std::endl;
+        printOptions();
         return 1;
     }
 
     int n = atoi(argv[1]);
-    std::cout<<" Compute pi for n= " << n <<": "<<std::endl;
-
-
+    std::cout<<" Computed pi for n= " << n <<": "<<std::endl;
     double pi = find_pi_machin(n);
-
     std::cout<< pi<<std::endl;
+
+    if (atoi(argv[2])==1){
+        utest();
+    }
+
+    if (atoi(argv[3])==1){
+        vtest();
+    }
 
 
     return 0;
